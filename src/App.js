dcf4ad6cd3d1e9c7df6780d783e4844a598f6151
Enrique <jruvalcaba1@ucol.mx>
2020-07-30 17:53:31 -0500

    App done - First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+
+// Components
+import Quote from "./components/Quote";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #1c7440 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: "Mulish", Arial, Helvetica, sans-serif;
+  color: #ffbe4d;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+  // State
+  const [quote, setQuote] = useState({});
+
+  const getApiData = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const quote = await api.json();
+    setQuote(quote[0]);
+  };
+
+  // useEffect
+  // Cargar una frase
+  useEffect(() => {
+    getApiData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Quote quote={quote} />
+      <Button onClick={getApiData}>Obtener frase</Button>
+    </Container>
   );
 }
 
---
+++ src/components/Quote.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "@emotion/styled";
+import PropTypes from "prop-types";
+
+const QuoteContainer = styled.div`
+  padding: 3em;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  max-width: 800px;
+
+  @media (min-width: 992px) {
+    margin-top: 10rem;
+  }
+
+  h1 {
+    font-family: "Mulish", Arial, Helvetica, sans-serif;
+    text-align: center;
+    position: relative;
+    padding-left: 4rem;
+
+    &::before {
+      content: open-quote;
+      font-size: 10rem;
+      color: black;
+      position: absolute;
+      left: -1rem;
+      top: -2rem;
+    }
+  }
+  p {
+    font-family: "Mulish", Arial, Helvetica, sans-serif;
+    font-size: 1.6rem;
+    font-weight: bold;
+    text-align: right;
+    color: #666;
+    margin-top: 2rem;
+  }
+`;
+
+const Quote = ({ quote }) => {
+  // Checar si el objeto contiene algo
+  //if (Object.keys(quote).length === 0) return null;
+
+  return (
+    <QuoteContainer>
+      <h1>{quote.quote}</h1>
+      <p>{quote.author}</p>
+    </QuoteContainer>
+  );
+};
+
+Quote.propTypes = {
+  quote: PropTypes.object.isRequired,
+};
+
+export default Quote;
